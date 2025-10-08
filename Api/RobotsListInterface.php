a3c2183a7d49b6ebdaa628d60d9c7d1572306c8d
Volodymyr Hryvinskyi <volodymyr@hryvinskyi.com>
2025-10-08 14:00:09 +0200

    Add new robots types

--- Api/RobotsListInterface.php
+++ Api/RobotsListInterface.php
@@ -14,6 +14,10 @@ interface RobotsListInterface
     public const NOINDEX_FOLLOW = 2;
     public const INDEX_NOFOLLOW = 3;
     public const INDEX_FOLLOW = 4;
+    public const NOINDEX_NOFOLLOW_NOARCHIVE = 5;
+    public const NOINDEX_FOLLOW_NOARCHIVE = 6;
+    public const INDEX_NOFOLLOW_NOARCHIVE = 7;
+    public const INDEX_FOLLOW_NOARCHIVE = 8;
 
     /**
      * @param int $code
